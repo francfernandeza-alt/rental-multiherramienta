dfabe48d505f7d903c0a6c493442a2d94af03cda
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-02 11:00:23 -0400

    Creados packages y clases usuarios

--- src/main/java/com/multiherramienta/multiherramienta/Model/UsuarioModel.java
+++ src/main/java/com/multiherramienta/multiherramienta/Model/UsuarioModel.java
@@ -1,7 +1,20 @@
+
+package com.multiherramienta.multiherramienta.Model;
+
+import jakarta.persistence.Table;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Usuario")
 
 public class UsuarioModel {
-
 
     private String nombreUsuario;
 }
---
+++ src/main/java/com/multiherramienta/multiherramienta/Services/UsuarioService.java
@@ -0,0 +1,8 @@
+package com.multiherramienta.multiherramienta.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+}
