f366af87d59cf3b069be52ce2c67a9ba3695c75b
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-02 08:54:20 -0400

    Agregado proyecto Spring Boot Multiherramienta

---
+++ src/main/java/com/multiherramienta/multiherramienta/MultiherramientaApplication.java
@@ -0,0 +1,13 @@
+package com.multiherramienta.multiherramienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiherramientaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiherramientaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multiherramienta/multiherramienta/MultiherramientaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiherramienta.multiherramienta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiherramientaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
