7b58b1e12ad57905474d4d6f88be850bdfcba66d
javier_olave <ja.olavea@duocuc.cl>
2026-05-06 18:00:10 -0400

    feature: añadir model material

---
+++ src/main/java/com/multiherramienta/multiherramienta/Model/Material.java
@@ -0,0 +1,43 @@
+package com.multiherramienta.multiherramienta.Model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Material")
+public class Material {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idMaterial;
+
+    @NotBlank(message = "El nombre del material es obligatorio")
+    @Size(min = 3, max = 100, message = "El nombre del material debe tener entre 3 y 100 caracteres")
+    @Column(nullable = false, length = 100)
+    private String nombreMaterial;
+
+    @NotBlank(message = "La descripción del material es obligatoria")
+    @Size(min = 3, max = 255, message = "La descripción del material debe tener entre 3 y 255 caracteres")
+    @Column(nullable = false, length = 255)
+    private String descripcionMaterial;
+
+    @OneToMany(mappedBy = "material")
+    private List<Materiales> materiales;
+}
