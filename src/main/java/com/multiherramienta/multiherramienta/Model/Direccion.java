b3bce2b76016f2b95a59ab10ce2555534c5aa74d
Nicolas_Labrin <ni.labrin@duocuc.cl>
2026-05-07 01:02:14 -0400

    Actualización de entidades

--- src/main/java/com/multiherramienta/multiherramienta/Model/Direccion.java
+++ src/main/java/com/multiherramienta/multiherramienta/Model/Direccion.java
@@ -1,5 +1,44 @@
 package com.multiherramienta.multiherramienta.Model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.multiherramienta.multiherramienta.Model.Usuario;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "Direccion")
 public class Direccion {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idDireccion;
+
+    @NotBlank(message = "La calle es obligatoria")
+    @Size(min = 3, max = 100)
+    @Column(nullable = false, length = 100)
+    private String calle;
+
+    @NotNull(message = "La numeración es obligatoria")
+    @Column(nullable = false)
+    private Integer numeracion;
+
+    @NotBlank(message = "La comuna es obligatoria")
+    @Size(min = 3, max = 100)
+    @Column(nullable = false, length = 100)
+    private String comuna;
+
+    @OneToOne
+    @JoinColumn(name = "rutUsuario")
+    private Usuario usuario;
 
 }
--- src/main/java/com/multiherramienta/multiherramienta/Model/TipoReserva.java
+++ src/main/java/com/multiherramienta/multiherramienta/Model/TipoReserva.java
@@ -1,5 +1,42 @@
 package com.multiherramienta.multiherramienta.Model;
+import java.time.LocalDate;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "Reserva")
 
 public class TipoReserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer numeroReserva;
 
+    @NotNull(message = "La fecha de inicio es obligatoria")
+    @Column(nullable = false)
+    private LocalDate fechaInicio;
+
+    @NotNull(message = "La fecha de fin es obligatoria")
+    @Column(nullable = false)
+    private LocalDate fechaFin;
+
+    @NotBlank(message = "El estado de la reserva es obligatorio")
+    @Column(nullable = false, length = 50)
+    private String estadoReserva;
+
+    @OneToOne
+    @JoinColumn(name = "rutUsuario")
+    private Usuario usuario;
+
+    @OneToOne
+    @JoinColumn(name = "idHerramienta")
+    private Herramienta herramienta;
 }
