07a8a1e30a13a7c366af50e619af1e137a1cdc9f
javier_olave <ja.olavea@duocuc.cl>
2026-05-07 18:50:59 -0400

    feat: añadir comuna service

---
+++ src/main/java/com/multiherramienta/multiherramienta/Services/ComunaService.java
@@ -0,0 +1,35 @@
+package com.multiherramienta.multiherramienta.Services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.multiherramienta.multiherramienta.Model.Comuna;
+import com.multiherramienta.multiherramienta.Repository.ComunaRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class ComunaService {
+
+    @Autowired
+    private ComunaRepository comunaRepository;
+
+    public List<Comuna> findAll() {
+        return comunaRepository.findAll();
+    }
+
+    public Comuna findById(Integer id) {
+        return comunaRepository.findById(id).orElse(null);
+    }
+
+    public Comuna save(Comuna comuna) {
+        return comunaRepository.save(comuna);
+    }
+
+    public void delete(Integer id) {
+        comunaRepository.deleteById(id);
+    }
+}
