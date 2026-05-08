bf55d7bb09f1f08196e4f186d7893b373b6e7a45
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-07 21:09:54 -0400

    actualizacion

--- src/main/java/com/multiherramienta/multiherramienta/Model/Material.java
+++ src/main/java/com/multiherramienta/multiherramienta/Model/Material.java
@@ -33,8 +33,6 @@ public class Material {
     @Column(nullable = false, length = 100)
     private String nombreMaterial;
 
-    @NotBlank(message = "La descripción del material es obligatoria")
-    @Size(min = 3, max = 255, message = "La descripción del material debe tener entre 3 y 255 caracteres")
     @Column(nullable = false, length = 255)
     private String descripcionMaterial;
 
