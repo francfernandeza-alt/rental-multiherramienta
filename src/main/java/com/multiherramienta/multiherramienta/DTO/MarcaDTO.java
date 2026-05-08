1ffe23fb8ce209777b2ab588798389aa7de4302b
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-08 18:22:14 -0400

    MarcaDTO

---
+++ src/main/java/com/multiherramienta/multiherramienta/DTO/MarcaDTO.java
@@ -0,0 +1,10 @@
+package com.multiherramienta.multiherramienta.DTO;
+
+import lombok.Data;
+
+@Data
+public class MarcaDTO {
+    private Integer idDireccionDTO;
+    private String direccionCompletaDTO;
+    private String nombreComunaDTO;
+}
