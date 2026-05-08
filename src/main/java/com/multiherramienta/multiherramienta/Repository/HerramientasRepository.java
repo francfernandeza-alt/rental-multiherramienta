61eed557d2c53591fa16935a8e67c89ae81ddf93
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-08 18:03:34 -0400

    correccion repositorio herramientas

--- src/main/java/com/multiherramienta/multiherramienta/Repository/HerramientasRepository.java
+++ src/main/java/com/multiherramienta/multiherramienta/Repository/HerramientasRepository.java
@@ -3,6 +3,8 @@ package com.multiherramienta.multiherramienta.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.multiherramienta.multiherramienta.Model.Herramientas;
+
 @Repository
 public interface HerramientasRepository extends JpaRepository <Herramientas, Integer> {
 
