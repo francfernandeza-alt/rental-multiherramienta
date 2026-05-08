f9d80e9e449758eacc75ab37d5cd7307481dee23
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-08 18:05:10 -0400

    correccion materiales

--- src/main/java/com/multiherramienta/multiherramienta/Model/Materiales.java
+++ src/main/java/com/multiherramienta/multiherramienta/Model/Materiales.java
@@ -1,8 +1,6 @@
 package com.multiherramienta.multiherramienta.Model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
