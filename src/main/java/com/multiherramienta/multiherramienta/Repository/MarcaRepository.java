6d10a5392d2852b0ff89e0cc033e71d73fee2d9e
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-08 18:02:30 -0400

    Correccion repositorio marca

--- src/main/java/com/multiherramienta/multiherramienta/Repository/MarcaRepository.java
+++ src/main/java/com/multiherramienta/multiherramienta/Repository/MarcaRepository.java
@@ -3,6 +3,8 @@ package com.multiherramienta.multiherramienta.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.multiherramienta.multiherramienta.Model.Marca;
+
 @Repository
 public interface MarcaRepository extends JpaRepository <Marca, Integer> {
 
