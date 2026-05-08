d69dd6ba492af1aadfd5d1bf07d660f1720cb75c
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-08 19:31:42 -0400

    Creacion de RegionService y correccion de RegionRepository

--- src/main/java/com/multiherramienta/multiherramienta/Repository/RegionRepository.java
+++ src/main/java/com/multiherramienta/multiherramienta/Repository/RegionRepository.java
@@ -1,8 +1,9 @@
 package com.multiherramienta.multiherramienta.Repository;
 
-import org.hibernate.cache.spi.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.multiherramienta.multiherramienta.Model.Region;
 
 @Repository
 public interface RegionRepository extends JpaRepository <Region, Integer> {
