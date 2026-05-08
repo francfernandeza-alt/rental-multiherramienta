1541a87ecf695d55ee7d6bebb082c0482d8947b2
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-08 14:12:23 -0400

    Creación de UsuarioDTO

--- src/main/java/com/multiherramienta/multiherramienta/DTO/UsuarioDTO.java
+++ src/main/java/com/multiherramienta/multiherramienta/DTO/UsuarioDTO.java
@@ -1,9 +1,17 @@
 package com.multiherramienta.multiherramienta.DTO;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 
 public class UsuarioDTO {
+    private String rutUsuarioDTO;
+    private String nombreUsuarioDTO;
+    private String apellidoPaternoDTO;
+    private String emailUsuarioDTO;
+    private String direccionUsuarioDTO;
+    private List<Integer> numeroReservas;
 
 }
