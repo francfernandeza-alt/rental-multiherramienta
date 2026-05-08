398e186a7b42f7d6666e46cee3dc5b7e83806644
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-08 17:56:39 -0400

    HerramientaDTO

---
+++ src/main/java/com/multiherramienta/multiherramienta/DTO/HerramientaDTO.java
@@ -0,0 +1,12 @@
+package com.multiherramienta.multiherramienta.DTO;
+
+import lombok.Data;
+
+@Data
+public class HerramientaDTO {
+    private Integer idHerramientaDAO;
+    private String nombreHerramientaDAO;
+    private String estadoHerramientaDAO;
+    private Integer cantidadDisponibleDAO;
+    private String nombreMarca;
+}
