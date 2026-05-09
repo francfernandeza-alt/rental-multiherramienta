ec1f0e2b05958de3257a1cfb70744ec7f4d6391a
javier_olave <ja.olavea@duocuc.cl>
2026-05-08 22:40:05 -0400

    feat: añadir material DTO

---
+++ src/main/java/com/multiherramienta/multiherramienta/DTO/MaterialDTO.java
@@ -0,0 +1,11 @@
+package com.multiherramienta.multiherramienta.DTO;
+
+import lombok.Data;
+
+@Data
+public class MaterialDTO {
+
+    private Integer idMaterial;
+    private String nombreMaterial;
+    private String descripcionMaterial;
+}
