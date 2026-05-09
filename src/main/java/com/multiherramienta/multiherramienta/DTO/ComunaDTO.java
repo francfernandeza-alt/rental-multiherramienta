18ab9e8be0b9b1a029a9421b8603d4694a94f299
javier_olave <ja.olavea@duocuc.cl>
2026-05-08 22:37:23 -0400

    feat: añadir comuna DTO

---
+++ src/main/java/com/multiherramienta/multiherramienta/DTO/ComunaDTO.java
@@ -0,0 +1,12 @@
+package com.multiherramienta.multiherramienta.DTO;
+
+import lombok.Data;
+
+@Data
+public class ComunaDTO {
+
+    private Integer idComuna;
+    private String nombreComuna;
+    private Integer numeroRegion;
+    private String nombreRegion;
+}
