153c6e8ddc5dbe5fddba7904ee3f465a6421760a
Franco Fernández Aguayo <franc.fernandeza@duocuc.cl>
2026-05-08 22:11:27 -0400

    correcciones en imports

--- src/main/java/com/multiherramienta/multiherramienta/Services/HerramientasService.java
+++ src/main/java/com/multiherramienta/multiherramienta/Services/HerramientasService.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.multiherramienta.multiherramienta.DTO.HerramientaDTO;
-import com.multiherramienta.multiherramienta.Model.Herramienta;
 import com.multiherramienta.multiherramienta.Model.Herramientas;
 import com.multiherramienta.multiherramienta.Repository.HerramientasRepository;
 
