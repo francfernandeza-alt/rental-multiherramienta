4ab72532a11e2afef0e5680ed962124ab5691ab2
javier_olave <ja.olavea@duocuc.cl>
2026-05-08 22:42:11 -0400

    feat: añadir tipoherramientas DTO

---
+++ src/main/java/com/multiherramienta/multiherramienta/DTO/TipoHerramientaDTO.java
@@ -0,0 +1,11 @@
+package com.multiherramienta.multiherramienta.DTO;
+
+import lombok.Data;
+
+@Data
+public class TipoHerramientaDTO {
+
+    private Integer idTipoHerramienta;
+    private String nombreTipoHerramienta;
+    private String descripcionTipoHerramienta;
+}
